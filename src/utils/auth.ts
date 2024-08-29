647e88580539ba67d2b63a650c95d9764ffe9a9b
Zacharie Bugaud <zacharie.bugaud@laposte.net>
2024-08-29 23:18:24 +0200

    refactor: Clear cookies in signOut function
    This commit refactors the `signOut` function in `auth.ts` to clear cookies using the `clearCookies` helper function. This ensures that all cookies are properly removed when a user signs out.

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -2,13 +2,31 @@ import { API_URL } from "../config";
 
 // import jwt from 'jsonwebtoken'; 
 
+
+function clearCookies() {
+    var cookies = document.cookie.split("; ");
+    for (var c = 0; c < cookies.length; c++) {
+        var d = window.location.hostname.split(".");
+        while (d.length > 0) {
+            var cookieBase = encodeURIComponent(cookies[c].split(";")[0].split("=")[0]) + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=' + d.join('.') + ' ;path=';
+            var p = window.location.pathname.split('/');
+            document.cookie = cookieBase + '/';
+            while (p.length > 0) {
+                document.cookie = cookieBase + p.join('/');
+                p.pop();
+            };
+            d.shift();
+        }
+    }
+}
+
 function signOut() {
     //clear the session
     //localStorage.removeItem('pro_until');
     localStorage.clear();
     //clear cookies
 //clear the session
-    document.cookie = 'session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT';
+clearCookies();
     //redirect to the homepage
     window.location.href = '/';
 
