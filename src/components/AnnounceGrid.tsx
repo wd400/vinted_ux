51de57562ed2be73c8a7f155070e4ba3e8fbfb2a
Zacharie Bugaud <zacharie.bugaud@laposte.net>
2024-09-21 17:38:39 +0200

    refactor: Update loading state management in AnnounceGrid component

--- src/components/AnnounceGrid.tsx
+++ src/components/AnnounceGrid.tsx
@@ -60,11 +60,11 @@ const AnnounceGrid: React.FC<AnnounceGridProps> = ({ catalogId, brandId }) => {
 
   return <>
   
-  {loading &&           <div className="flex justify-center items-center h-64">
+  {loading.current &&           <div className="flex justify-center items-center h-64">
             <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-primary"></div>
           </div>
           }
-  {!loading && <AnnounceGridDisplay announces={announces} />}
+  {!loading.current && <AnnounceGridDisplay announces={announces} />}
   </>
 };
 
